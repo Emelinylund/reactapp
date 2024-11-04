11ed6547b91dac23a2a015374705e4abd55ec3ff
Emeli Nylund <emsnylund@hotmail.com>
2024-11-04 16:34:00 +0100

    changed the star image to one star only and done with the API testimonials

--- react/src/components/Page5.jsx
+++ react/src/components/Page5.jsx
@@ -1,15 +1,34 @@
 import React, { useState, useEffect } from 'react'
-import Quoteicon from '../images/quotes.svg'
-import Stars from '../images/rating.svg'
-import Fannie from '../images/Fannie.svg'
-import Stars2 from '../images/rating (1).svg'
-import Albert from '../images/albert.svg'
+import QuoteIcon from '../images/quotes.svg'
+import StarIcon from '../images/star.svg' 
+
+const ClientCard = ({ comment, avatarUrl, author, jobRole, starRating }) => {
+  return (
+    <div className="clients-card">
+      <div className="quote-icon">
+        <img src={QuoteIcon} alt="quote-icon" />
+      </div>
+      <div className="stars" style={{ display: 'flex' }}>
+        {Array.from({ length: starRating }, (_, index) => (
+          <img key={index} src={StarIcon} alt="star" style={{ marginRight: '4px' }} />
+        ))}
+      </div>
+      <p>{comment}</p>
+      <div className="client-info">
+        <img src={avatarUrl} alt={`${author}'s avatar`} className="client-photo" />
+        <div>
+          <h4>{author}</h4>
+          <p>{jobRole}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const Page5 = () => {
-  const [testimonials, setTestimonials] = useState([])
+  const [testimonials, setTestimonials] = useState([]);
 
   useEffect(() => {
-
     const fetchTestimonials = async () => {
       try {
         const res = await fetch('https://win24-assignment.azurewebsites.net/api/testimonials')
@@ -18,10 +37,11 @@ const Page5 = () => {
       } catch (error) {
         console.error('Error fetching testimonials:', error)
       }
-    };
+    }
 
     fetchTestimonials()
-  }, []);
+  }, [])
+
   return (
     <section id="page5">
       <div className="container6">
@@ -29,47 +49,22 @@ const Page5 = () => {
           <h2>Clients are <br />Loving Our App</h2>
         </div>
         <div className="clients-grid">
-          <div className="clients-card">
-            <div className="quote-icon">
-              <img src={Quoteicon} alt="quote-icon" />
-            </div>
-            <div className="stars">
-              <img src={Stars} alt="stars" />
-            </div>
-            <p>
-              Sit pretium aliquam tempor, orci dolor sed maecenas rutrum sagittis. Laoreet posuere rhoncus, egestas lacus, egestas justo aliquam vel. Nisi vitae lectus hac hendrerit. Montes justo turpis sit amet.
-            </p>
-            <div className="client-info">
-              <img src={Fannie} alt="Fannie Summers" className="client-photo" />
-              <div>
-                <h4>Fannie Summers</h4>
-                <p>Designer</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="clients-card">
-            <div className="quote-icon">
-              <img src={Quoteicon} alt="quote-icon" />
-            </div>
-            <div className="stars">
-              <img src={Stars2} alt="stars" />
-            </div>
-            <p>
-              Nunc senectus leo vel venenatis accumsan vestibulum sollicitudin amet porttitor. Nisl bibendum nulla tincidunt eu enim ornare dictumst sit amet. Dictum pretium dolor tincidunt egestas eget nunc.
-            </p>
-            <div className="client-info">
-              <img src={Albert} alt="Albert Flores" className="client-photo" />
-              <div>
-                <h4>Albert Flores</h4>
-                <p>Developer</p>
-              </div>
-            </div>
-          </div>
+          {testimonials.map((testimonial) => (
+            <ClientCard
+              key={testimonial.id}
+              comment={testimonial.comment}
+              avatarUrl={testimonial.avatarUrl}
+              author={testimonial.author}
+              jobRole={testimonial.jobRole}
+              starRating={testimonial.starRating}
+            />
+          ))}
         </div>
       </div>
     </section>
-  );
+  )
 }
 
 export default Page5
+
+
