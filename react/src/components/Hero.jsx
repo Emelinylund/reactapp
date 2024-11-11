2f1dafd65cba5f79410fbbc22db03984d90eebdb
Emeli Nylund <emsnylund@hotmail.com>
2024-11-11 15:05:02 +0100

    fixed pictures in darkmode

--- react/src/components/Hero.jsx
+++ react/src/components/Hero.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import Appstore from '../images/appstore (2).svg'
-import Googleplay from '../images/googleplay (2).svg'
-import Phoneback from '../images/iphone 12 Probakom1.svg'
-import Phonefront from '../images/iphone 12 Pro.svg'
+import React, { useEffect, useState } from 'react';
+import AppstoreLight from '../images/appstore (2).svg';
+import GoogleplayLight from '../images/googleplay (2).svg';
+import AppstoreDark from '../images/Appstoredarkmode.svg';
+import GoogleplayDark from '../images/googleplaydarkmode.svg';
+import Phoneback from '../images/iphone 12 Probakom1.svg';
+import Phonefront from '../images/iphone 12 Pro.svg';
 
 const Hero = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const checkDarkMode = () => {
+      setIsDarkMode(document.body.classList.contains('dark-theme'));
+    };
+
+    const observer = new MutationObserver(checkDarkMode);
+    observer.observe(document.body, { attributes: true, attributeFilter: ['class'] });
+
+    checkDarkMode();
+
+    return () => observer.disconnect();
+  }, []);
+
+  // Använd dark mode-bilder om isDarkMode är true, annars light mode-bilder för App Store och Google Play
+  const AppstoreImage = isDarkMode ? AppstoreDark : AppstoreLight;
+  const GoogleplayImage = isDarkMode ? GoogleplayDark : GoogleplayLight;
+
   return (
     <section id="hero1">
       <div className="container1"> 
@@ -13,8 +34,8 @@ const Hero = () => {
           <div className="content"> 
             <p>We offer you a new generation of the mobile banking. Save, spend & manage money in your pocket.</p>
             <div className="buttons"> 
-              <a className="btn-download-app" href="#"><img src={Appstore} alt="appstore" /></a>
-              <a className="btn-download-app" href="#"><img src={Googleplay} alt="googleplay" /></a>
+              <a className="btn-download-app" href="#"><img src={AppstoreImage} alt="appstore" /></a>
+              <a className="btn-download-app" href="#"><img src={GoogleplayImage} alt="googleplay" /></a>
             </div>
             <a href="#" className="discover-more"> 
               <span className="btn-circle">
@@ -34,4 +55,4 @@ const Hero = () => {
   );
 }
 
-export default Hero
+export default Hero;
